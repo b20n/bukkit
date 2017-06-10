12004e3e7464c32c6b31c353592d482ecb186ab3
Benjamin Anderson <b@banjiewen.net>
2017-06-09 17:11:54 -0700

    Collapse to a standalone app

---
+++ c_src/bukkit_hdr.d
@@ -0,0 +1,1 @@
+c_src/bukkit_hdr.o: c_src/bukkit_hdr.c c_src/bukkit_hdr.h
---
+++ c_src/bukkit_hdr_nif.d
@@ -0,0 +1,6 @@
+c_src/bukkit_hdr_nif.o: c_src/bukkit_hdr_nif.c \
+  /usr/local/Cellar/erlang/19.2.3/lib/erlang/erts-8.2.2/include/erl_nif.h \
+  /usr/local/Cellar/erlang/19.2.3/lib/erlang/erts-8.2.2/include/erl_drv_nif.h \
+  /usr/local/Cellar/erlang/19.2.3/lib/erlang/erts-8.2.2/include/erl_int_sizes_config.h \
+  /usr/local/Cellar/erlang/19.2.3/lib/erlang/erts-8.2.2/include/erl_nif_api_funcs.h \
+  c_src/bukkit_hdr.h
